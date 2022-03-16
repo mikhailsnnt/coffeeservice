30952b1ee651244d64fd777c795f4493da8ab449
Mihail Nikitin <nitinin.miha@icloud.com>
2022-03-17 01:19:11 +0300

    Updated CoffeePreparationService:  Added prepareCoffe(Name) method,  Added logging,  Put ingredient entity saving.

--- src/main/java/com/sainntt/coffeapi/service/CoffeePreparationService.java
+++ src/main/java/com/sainntt/coffeapi/service/CoffeePreparationService.java
@@ -3,5 +3,7 @@ package com.sainntt.coffeapi.service;
 import com.sainntt.coffeapi.dto.CoffeePreparationResult;
 
 public interface CoffeePreparationService {
-    void prepareCoffee(int id);
+    CoffeePreparationResult prepareCoffee(int id);
+
+    CoffeePreparationResult prepareCoffee(String name);
 }
--- src/main/java/com/sainntt/coffeapi/service/impl/CoffeePreparationServiceImpl.java
+++ src/main/java/com/sainntt/coffeapi/service/impl/CoffeePreparationServiceImpl.java
@@ -1,35 +1,54 @@
 package com.sainntt.coffeapi.service.impl;
 
+import com.sainntt.coffeapi.dto.CoffeePreparationResult;
 import com.sainntt.coffeapi.entity.Coffee;
 import com.sainntt.coffeapi.entity.Ingredient;
-import com.sainntt.coffeapi.exception.NotEnoughIngredientException;
 import com.sainntt.coffeapi.exception.CoffeeNotFoundException;
+import com.sainntt.coffeapi.exception.NotEnoughIngredientException;
 import com.sainntt.coffeapi.repository.CoffeeRepository;
+import com.sainntt.coffeapi.repository.IngredientRepository;
 import com.sainntt.coffeapi.service.CoffeePreparationService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+@Slf4j
 @Service
 public class CoffeePreparationServiceImpl implements CoffeePreparationService {
     private final CoffeeRepository coffeeRepository;
+    private final IngredientRepository ingredientRepository;
 
     @Autowired
-    public CoffeePreparationServiceImpl(CoffeeRepository coffeeRepository) {
+    public CoffeePreparationServiceImpl(CoffeeRepository coffeeRepository, IngredientRepository ingredientRepository) {
         this.coffeeRepository = coffeeRepository;
+        this.ingredientRepository = ingredientRepository;
+    }
+
+    @Override
+    public CoffeePreparationResult prepareCoffee(int id) {
+        if (!coffeeRepository.existsById(id))
+            throw new CoffeeNotFoundException(id);
+        return prepareCoffee(coffeeRepository.getById(id));
+    }
+
+    @Override
+    public CoffeePreparationResult prepareCoffee(String name) {
+        return prepareCoffee(coffeeRepository.getByName(name)
+                .orElseThrow(() -> new CoffeeNotFoundException(name)));
     }
 
     @Transactional
-    @Override
-    public void prepareCoffee(int id) {
-        if (!coffeeRepository.existsById(id))
-            throw new CoffeeNotFoundException(id);
-        Coffee coffee = coffeeRepository.getById(id);
+    public CoffeePreparationResult prepareCoffee(Coffee coffee) {
+        log.info("Preparing coffee {}",coffee);
         coffee.getIngredients().forEach(receiptElement -> {
             Ingredient ingredient = receiptElement.getIngredient();
             if (ingredient.getAmountLeft() < receiptElement.getAmount())
                 throw new NotEnoughIngredientException(ingredient.getName(), receiptElement.getAmount() - ingredient.getAmountLeft());
             ingredient.setAmountLeft(ingredient.getAmountLeft() - receiptElement.getAmount());
+            ingredientRepository.save(ingredient);
         });
+        return new CoffeePreparationResult(coffee.getId(), coffee.getName());
+
     }
 }
