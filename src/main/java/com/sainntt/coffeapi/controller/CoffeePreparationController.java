d06a1b40bd00c177bc82711909efc1299c5d8c5a
Mihail Nikitin <nitinin.miha@icloud.com>
2022-03-17 01:32:38 +0300

    Implemented CoffeePreparationController

---
+++ src/main/java/com/sainntt/coffeapi/controller/CoffeePreparationController.java
@@ -0,0 +1,27 @@
+package com.sainntt.coffeapi.controller;
+
+import com.sainntt.coffeapi.dto.CoffeePreparationResult;
+import com.sainntt.coffeapi.service.CoffeePreparationService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(name = "/coffee/prepare")
+public class CoffeePreparationController {
+    private final CoffeePreparationService service;
+
+    @Autowired
+    public CoffeePreparationController(CoffeePreparationService service) {
+        this.service = service;
+    }
+
+    @GetMapping("/{coffeeId}")
+    public ResponseEntity<CoffeePreparationResult> getCoffee(@PathVariable int coffeeId){
+         return ResponseEntity.ok(service.prepareCoffee(coffeeId));
+    }
+
+}
