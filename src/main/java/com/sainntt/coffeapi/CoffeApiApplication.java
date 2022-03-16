4247e5b281db19045088f5766ed997b30a39d563
Mihail Nikitin <nitinin.miha@icloud.com>
2022-03-16 15:36:37 +0300

    Initial commit

---
+++ src/main/java/com/sainntt/coffeapi/CoffeApiApplication.java
@@ -0,0 +1,13 @@
+package com.sainntt.coffeapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoffeApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CoffeApiApplication.class, args);
+    }
+
+}
