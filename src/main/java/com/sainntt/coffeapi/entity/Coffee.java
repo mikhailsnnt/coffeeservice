85c0d8f982a7dc7746296f06b7e52ee684e0f654
Mihail Nikitin <nitinin.miha@icloud.com>
2022-03-17 01:16:24 +0300

    Updated entities

--- src/main/java/com/sainntt/coffeapi/entity/Coffee.java
+++ src/main/java/com/sainntt/coffeapi/entity/Coffee.java
@@ -2,13 +2,16 @@ package com.sainntt.coffeapi.entity;
 
 import lombok.Getter;
 import lombok.Setter;
+import lombok.ToString;
 
 import javax.persistence.*;
+import java.util.ArrayList;
 import java.util.List;
 
 @Entity
 @Getter
 @Setter
+@ToString(exclude = "ingredients")
 public class Coffee {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -18,5 +21,5 @@ public class Coffee {
             fetch = FetchType.EAGER,
             mappedBy = "coffee"
     )
-    private List<RecipeElement> ingredients;
+    private List<RecipeElement> ingredients = new ArrayList<>();
 }
--- src/main/java/com/sainntt/coffeapi/entity/RecipeElement.java
+++ src/main/java/com/sainntt/coffeapi/entity/RecipeElement.java
@@ -8,13 +8,16 @@ import javax.persistence.*;
 @Entity
 @Getter
 @Setter
+@Table(uniqueConstraints = @UniqueConstraint(columnNames = {"coffee_id","ingredient_id"}))
 public class RecipeElement {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
     @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "coffee_id",nullable = false)
     private Coffee coffee;
     private long amount;
     @OneToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "ingredient_id",nullable = false)
     private Ingredient ingredient;
 }
