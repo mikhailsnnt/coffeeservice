6e82058125b90f99baab4a0e5246786bf1c206e1
Mihail Nikitin <nitinin.miha@icloud.com>
2022-03-17 01:50:20 +0300

    Implemented CoffeeInformationController

---
+++ src/main/java/com/sainntt/coffeapi/controller/CoffeeInformationController.java
@@ -0,0 +1,31 @@
+package com.sainntt.coffeapi.controller;
+
+import com.sainntt.coffeapi.dto.CoffeeRecipeDto;
+import com.sainntt.coffeapi.service.CoffeeRecipeService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class CoffeeInformationController {
+    private final CoffeeRecipeService recipeService;
+
+    public CoffeeInformationController(CoffeeRecipeService recipeService) {
+        this.recipeService = recipeService;
+    }
+
+    @GetMapping("/coffee")
+    public ResponseEntity<List<CoffeeRecipeDto>> getRecipes() {
+        return ResponseEntity.ok(recipeService.getRecipes());
+    }
+
+    @GetMapping("/coffee/{coffeeId}")
+    public ResponseEntity<CoffeeRecipeDto> getRecipe(@PathVariable int coffeeId) {
+        return ResponseEntity.ok(recipeService.getRecipe(coffeeId));
+    }
+
+
+}
